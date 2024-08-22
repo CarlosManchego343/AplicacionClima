daffc2806c9d79c7a68b0accd3f93980a96c84ad
Wendigo43 <carlosmariomanchego1@gmail.com>
2024-08-22 15:59:58 -0500

    Update-Location permissions-CMMP

--- lib/screens/principal_screen.dart
+++ lib/screens/principal_screen.dart
@@ -1,5 +1,6 @@
 import 'package:aplicacion_clima/screens/forecasr_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class PrincipalScreen extends StatefulWidget {
   @override
@@ -7,30 +8,69 @@ class PrincipalScreen extends StatefulWidget {
 }
 
 class _PrincipalScreenState extends State<PrincipalScreen> {
+  LocationData? _currentLocation;
+  Location location = Location();
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+  }
+
+  Future<void> _getLocation() async {
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    final locationData = await location.getLocation();
+    setState(() {
+      _currentLocation = locationData;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Clima actual"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('Información 1'),
-          Text('Información 2'),
-          Text('Información 3'),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ForecastScreen()));
-              },
-              child: const Text("Pronóstico en 3 días"),
+      body: _currentLocation == null
+          ? CircularProgressIndicator()
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                'Latitude: ${_currentLocation!.latitude}, Longitude: ${_currentLocation!.longitude}',
+              ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ForecastScreen()));
+                    },
+                    child: const Text("Pronóstico en 3 días"),
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
